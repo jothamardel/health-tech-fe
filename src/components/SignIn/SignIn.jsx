7a9d89a044f0e1854c8c054cd3e50afa6ff797df
kaushalluffa <webdevelopment852@gmail.com>
2022-10-14 15:48:43 -0400

    signup and signin component complete

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import './SignIn.scss'
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div>SignIn</div>
-  )
+    <div className="signin">
+      <div className="signin__card">
+        <div className="signin__card--header">
+          <h1>Sign Up</h1>
+        </div>
+        <div className="signin__card--input">
+          <label className="label">Email:</label>
+          <input
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <label className="label">Password:</label>
+          <input
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+        </div>
+        <div className="signin__card--footer">
+          <button className="signup__btn"><Link to="/signup">Sign Up</Link></button>
+          <button className="signin__btn">Sign In</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import './SignUp.scss'
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-export default SignUp
+  return (
+    <div className="signup">
+      <div className="signup__card">
+        <div className="signup__card--header">
+          <h1>Sign Up</h1>
+        </div>
+        <div className="signup__card--input">
+          <label className="label">Email:</label>
+          <input
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          />
+          <label className="label">Password:</label>
+          <input
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <label className="label">Confirm Password:</label>
+          <input
+            type="password"
+            required
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Confirm Password"
+          />
+        </div>
+        <div className="signup__card--footer">
+          <button className="signin__btn"><Link to="/signin">Sign In</Link></button>
+          <button className="signup__btn">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
