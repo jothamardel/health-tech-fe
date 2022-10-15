25ca4cadea975175e24b05e1709e7c28e7b9316e
jothamardel <ardelmbiplang@gmail.com>
2022-10-16 00:29:02 +0100

    chore: updating doctor request access

--- src/components/ActivePatients/ActivePatients.jsx
+++ src/components/ActivePatients/ActivePatients.jsx
@@ -1,8 +1,45 @@
 import "./ActivePatients.scss";
+import axios from "axios";
 
 
 function ActivePatients(props) {
-  const { patients } = props;
+  const { patients, httpGetAllUnWell } = props;
+
+  function checkIfDoctorIsPermitted(arr = []) {
+    if (!arr.length) {
+      return "Request access"
+    }
+    const { _id } = JSON.parse(localStorage.getItem('user'));
+    const newArr = [...arr];
+    const findDoc = newArr.find(item => item.id === _id)
+
+    if (!findDoc) {
+      return "Request access"
+    }
+
+    if (findDoc && !findDoc.allowed) {
+      return "Pending approval"
+    }
+    return findDoc.allowed;
+  }
+
+  async function httpRequestAccess(user) {
+    const { _id, fullName } = JSON.parse(localStorage.getItem('user'));
+    user.permissions.push({
+      fullName,
+      id: _id,
+      allowed: false
+    })
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_UNIFY_API_URL}api/unwell`, user);
+      console.log(response.data.message);
+      httpGetAllUnWell();
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+
+
   return (
     <>
       <div className="active-patient-input">
@@ -15,8 +52,14 @@ function ActivePatients(props) {
             <div className="card-patient" key={item._id}>
               <h1>{item.full_name}</h1>
               <div>
-                <button>start diagnosis</button>
-                <button>discharge</button>
+                <button onClick={() => httpRequestAccess(item)}>{checkIfDoctorIsPermitted(item.permissions)}</button>
+                {
+                  (checkIfDoctorIsPermitted() === true) &&
+                  <>
+                    <button>start diagnosis</button>
+                    <button>discharge</button>
+                  </>
+                }
               </div>
             </div>
             ))
