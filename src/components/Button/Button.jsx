a4af4306661b01ce9cded6aea8bb1deafd66a40e
jothamardel <ardelmbiplang@gmail.com>
2022-10-15 16:12:02 +0100

    feat: implementing custom button

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Button.scss"
+
+const Button = () => {
+  return (
+    <div className='main-container'>
+      <div className=''>
+          <button>I'm not feeling well!</button>
+      </div>
+    </div>
+  )
+}
+
+export default Button;
