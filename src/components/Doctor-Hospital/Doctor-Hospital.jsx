95c6d7a385011db8e761983fe39833d1dd7a0b45
jothamardel <ardelmbiplang@gmail.com>
2022-10-15 01:23:50 +0100

    feat: implementing doctor/hospital dashboard

--- src/components/Doctor-Hospital/Doctor-Hospital.jsx
+++ src/components/Doctor-Hospital/Doctor-Hospital.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./Doctor-Hospital.scss"
 
 const DoctorHospital = () => {
+  const [section, setSection] = useState('active_patients');
+
+  const showPage = {
+    active_patients: null,
+    past_patients: null,
+    log_patients: null,
+    approvals: null
+  }
+
+
   return (
-    <div>Doctor-Hospital</div>
+    <div className='doc-container'>
+      <h2>Doctor/Hospital Dashboard</h2>
+      <div className='d-dashboard'>
+        <nav className='nav-tab'> 
+          <ul>
+            <li>Active Patients</li>
+            <li>Past Patients</li>
+            <li>Log Patients</li>
+            <li className='list'>Approvals</li>
+          </ul>
+        </nav>
+      </div>
+      {showPage[section]}
+    </div>
   )
 }
 
