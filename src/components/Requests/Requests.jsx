c8d38a8496fd367cb01dbb154ffc078b6de42a3a
jothamardel <ardelmbiplang@gmail.com>
2022-10-16 12:27:56 +0100

    chore: updating request page

--- src/components/Requests/Requests.jsx
+++ src/components/Requests/Requests.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 import "./Requests.scss";
 
 const Requests = () => {
-  const { _id } = JSON.parse(localStorage.getItem("user"));
+  const { _id, fullName } = JSON.parse(localStorage.getItem("user"));
   const [fetchedData, setFetchedData] = useState(null);
+  const [permissions, setPermissions] = useState([]);
   useEffect(() => {
     const config = {
       method: "get",
@@ -37,80 +38,100 @@ const Requests = () => {
         console.log(err);
       });
   }
-  function approveRequest() {
-    const permissionsObj = [
-      {
-        fullName: fetchedData?.permissions[0]?.fullName,
-        id: fetchedData?.permissions[0]?.id,
-        allowed: true,
-      },
-      {
-        fullName: "Tom",
-        id: "634b5e7550c8f9ab6b4d66c8",
-        allowed: true,
-      },
-    ];
-    const config = {
-      method: "post",
-      url: `https://care-system.herokuapp.com/api/unwell/`,
-      headers: {},
-      data: { ...fetchedData, permissions: [...permissionsObj] },
-    };
-    axios(config)
-      .then(function (res) {
-        localStorage.setItem('user', JSON.stringify(res.data.data[0]))
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
+  function approveRequest(doctor) {
+    console.log(doctor)
+    const newArr = [...permissions];
+    const doc = newArr[0].permissions.find(item => item.id === doctor.id)
+    doc.allowed = true;
+    console.log(newArr);
+    httpUpdatePatientPermissions(newArr[0]);
+    // const permissionsObj = [
+    //   {
+    //     fullName: fetchedData?.permissions[0]?.fullName,
+    //     id: fetchedData?.permissions[0]?.id,
+    //     allowed: true,
+    //   },
+    //   {
+    //     fullName: "Tom",
+    //     id: "634b5e7550c8f9ab6b4d66c8",
+    //     allowed: true,
+    //   },
+    // ];
+    // const config = {
+    //   method: "post",
+    //   url: `https://care-system.herokuapp.com/api/unwell/`,
+    //   headers: {},
+    //   data: { ...fetchedData, permissions: [...permissionsObj] },
+    // };
+    // axios(config)
+    //   .then(function (res) {
+    //     localStorage.setItem('user', JSON.stringify(res.data.data[0]))
+    //   })
+    //   .catch(function (err) {
+    //     console.log(err);
+    //   });
   }
  
+  async function httpGetPatientPermissions() {
+    const user =  JSON.parse(localStorage.getItem("user"));
+    if (!user) return;
+    const { _id } = user;
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_UNIFY_API_URL}api/unwell/${_id}`);
+      // console.log(response.data)
+      setPermissions(response.data.data)
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+  async function httpUpdatePatientPermissions(data) {
+   
+    try {
+      const response = await axios.post(`${process.env.REACT_APP_UNIFY_API_URL}api/unwell`, data);
+      console.log(response.data.data)
+      // setPermissions(response.data.data)
+      httpGetPatientPermissions();
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+
+  useEffect(() => {
+    httpGetPatientPermissions();
+  }, [])
+
+
+  function getJustPermissions(arr = []) {
+    const newArr = [...arr];
+    const filteredPermissions = newArr.reduce((acc, arr) => {
+      let value = acc.concat(arr.permissions)
+      return value;
+    }, [])
+    return filteredPermissions;
+  }
+  
 
   return (
     
     <div className="requests">
-      {fetchedData ? (
-        <div className="requests__card">
-          <h2 className="requests__card--heading">
-            
-            {!fetchedData?.permissions[0]?.allowed ?
-              `You have a request to approve.`:`You don't have any requets pending` }
-          </h2>
-          {!fetchedData?.permissions[0]?.allowed ? (
-            <>
-              <p>Hi, {fetchedData.permissions[0].fullName}</p>
-              <p>
-                Doctor {fetchedData?.permissions[1]?.fullName} is requesting get
-                access to your medical history and profile.
-              </p>
-              <p>If you authorized this please click 'Approve'.</p>
-              <p>If not please click on 'Reject'</p>
-            </>
-          ): (
-            <>
-              <p>Sorry, you dont have any requests pending.</p>
-              <p>Please refresh and try again.</p>
-            </>
-          )}
-          <div className="requests__card--cta">
-            {fetchedData?.permissions[0]?.allowed && (
-              <button className="approve">
-                <Link to="/">Go to Home</Link>
+      {
+        getJustPermissions(permissions).map(item => (
+          <div className="requests__card" key={item.id}>
+            <h2 className="requests__card--heading">You have a request to approve.</h2>
+            <p>Hi, {fullName}</p>
+            <p>Doctor {item.fullName} is requesting get access to your medical history and profile.</p>
+            <p>If you authorized this please click 'Approve'.</p>
+            <p>If not please click on 'Reject'</p>
+            <div className="requests__card--cta">
+              <button className="approve" onClick={() => approveRequest(item)}>
+                Approve
               </button>
-            )}
-            {!fetchedData?.permissions[0]?.allowed && (
-              <>
-                <button className="approve" onClick={approveRequest}>
-                  Approve
-                </button>
-                <button className="reject" onClick={rejectRequest}>
-                  Reject
-                </button>
-              </>
-            )}
+              <button className="reject" onClick={rejectRequest}>
+                Reject
+              </button>
+            </div>
           </div>
-        </div>
-      ): <p style={{fontSize:'2rem'}}>Loading</p>}
+      ))}
     </div>
   );
 };
